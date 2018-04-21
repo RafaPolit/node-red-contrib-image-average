000f2a2a8663ce6fc9ace0cd4050168f762339ad
RafaPolit <rafaelpolit@gmail.com>
2018-04-21 15:18:37 -0500

    Extracting all pixel info.

--- image-average.js
+++ image-average.js
@@ -4,7 +4,29 @@ const input = (node, msg, config) => {
   node.warn('Config:' + JSON.stringify(config, null, ' '));
   node.warn('msg:' + JSON.stringify(msg, null, ' '));
   node.warn('jimp?:' + Jimp);
-  node.send(msg);
+
+  const URL = config.URL || msg.payload;
+  const rValues = [];
+  const gValues = [];
+  const bValues = [];
+
+  node.warn('URL:' + URL);
+  Jimp.read(URL)
+  .then(function (img) {
+    node.warn('Acquired image');
+    img.scan(0, 0, img.bitmap.width, img.bitmap.height, function (x, y, idx) {
+      rValues.push(this.bitmap.data[ idx + 0 ]);
+      gValues.push(this.bitmap.data[ idx + 1 ]);
+      bValues.push(this.bitmap.data[ idx + 2 ]);
+    });
+
+    msg.payload = { r: rValues, g: gValues, b: bValues };
+
+    node.send(msg);
+  }).catch(function (err) {
+    node.warn(err);
+  });
+
 }
 
 module.exports = function(RED) {
