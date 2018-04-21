74baeac0381f63cb07830ee1bdd3b08d0fc6a572
RafaPolit <rafaelpolit@gmail.com>
2018-04-21 14:50:45 -0500

    Configured start node for testing.

---
+++ image-average.js
@@ -0,0 +1,18 @@
+const Jimp = require('jimp');
+
+const input = (node, msg, config) => {
+  node.warn('Config:', config);
+  node.warn('msg:', msg);
+  node.warn('jimp?:', Jimp);
+  node.send(msg);
+}
+
+module.exports = function(RED) {
+  function ImageAverageNode(config) {
+    RED.nodes.createNode(this, config);
+    let node = this;
+    node.on('input', (msg) => { input(node, data, config); });
+  }
+
+  RED.nodes.registerType('image-average', ImageAverageNode);
+}
