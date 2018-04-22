8ef741d3c480906c3615c4b9037704e474612694
RafaPolit <rafaelpolit@gmail.com>
2018-04-22 16:03:38 -0500

    Moved logic to a single loop, added stats.

--- image-average.js
+++ image-average.js
@@ -1,24 +1,26 @@
 const Jimp = require('jimp');
 
 const input = (node, msg, config) => {
-  node.warn('Config:' + JSON.stringify(config, null, ' '));
-  node.warn('msg:' + JSON.stringify(msg, null, ' '));
-  node.warn('jimp?:' + Jimp);
-
   const URL = config.URL || msg.payload;
-  const values = [];
 
-  node.warn('URL:' + URL);
+  let t = process.hrtime();
+
   Jimp.read(URL)
   .then(function (img) {
-    node.warn('Acquired image');
+    const values = [0, 0, 0];
     img.scan(0, 0, img.bitmap.width, img.bitmap.height, function (x, y, idx) {
-      values.push([ this.bitmap.data[ idx + 0 ], this.bitmap.data[ idx + 1 ], this.bitmap.data[ idx + 2 ] ]);
+      values = [
+        values[0] + this.bitmap.data[ idx + 0 ],
+        values[1] + this.bitmap.data[ idx + 1 ],
+        values[2] + this.bitmap.data[ idx + 2 ]
+      ]);
     });
 
-    const avg = values.reduce((a,b) => ([a[0] + b[0], a[1] + b[1], a[2] + b[2]]), [0, 0, 0]);
-
     msg.payload = avg.map(v => v / values.length);
+
+    t = process.hrtime(t);
+
+    msg.stats = { pixels: values.length, time: t[0] + t[1] / 1000000000 }
 
     node.send(msg);
   }).catch(function (err) {
